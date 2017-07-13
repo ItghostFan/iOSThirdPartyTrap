0813c69a75f32de87b6ccb1838340962eb870cc6
ItghostFan <ItghostFan@163.com>
2017-07-13 18:50:17 +0800

    Remove some invalid file

--- Example/Pods/Target Support Files/Pods-iOSThirdPartyTrap_Example/Pods-iOSThirdPartyTrap_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-iOSThirdPartyTrap_Example/Pods-iOSThirdPartyTrap_Example-frameworks.sh
@@ -96,13 +96,6 @@ strip_invalid_archs() {
   fi
 }
 
-
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/iOSThirdPartyTrap/iOSThirdPartyTrap.framework"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/iOSThirdPartyTrap/iOSThirdPartyTrap.framework"
-fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
 fi
--- Example/Pods/Target Support Files/iOSThirdPartyTrap/iOSThirdPartyTrap-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_iOSThirdPartyTrap : NSObject
-@end
-@implementation PodsDummy_iOSThirdPartyTrap
-@end
